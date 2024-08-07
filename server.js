620f7b58be1e65bd6a97a5868cce4509350eaeab
OtegaClinton <oghenetegaokotie@gmail.com>
2024-08-07 11:12:24 +0100

    adding cors

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require ("dotenv").config();
 const express = require("express");
 require('./config/database');
 const router = require('./router/userRouter')
+const cors = require("cors");
 
 const app = express();
+app.use(cors("*"));
 app.use(express.json());
 app.use("/api/v1/",router);
 
